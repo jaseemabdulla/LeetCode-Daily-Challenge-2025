86d7c51c9904e1242f1a3add0669f1dd2d77f6a3
jaseem <jaseempk003@gmail.com>
2025-02-25 22:46:38 +0530

    added try

---
+++ ja.py
@@ -0,0 +1,14 @@
+def numOfSubarrays(arr):
+    res = 0
+    odd = 0
+    even = 0
+    csum = 0
+    for num in arr:
+        csum += num
+        if csum % 2:
+            res += 1 + even
+            odd += 1
+        else:
+            res += odd
+            even += 1
+    return res % (10**9 + 7)
